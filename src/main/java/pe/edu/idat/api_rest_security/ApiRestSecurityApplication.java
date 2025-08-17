1a9d5b2ac92934a1c9187a93cb6b70e505e9f6e5
richardsamillan <richiander24@gmail.com>
2025-08-17 01:26:16 -0500

    primer commit:subiendo proyecto

---
+++ src/main/java/pe/edu/idat/api_rest_security/ApiRestSecurityApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.api_rest_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/api_rest_security/ApiRestSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.api_rest_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
